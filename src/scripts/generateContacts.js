79efe2881d8a99264c7e02ec94700580ac865c2e
Andrii-Petrunia <petrunyandrey@gmail.com>
2025-02-23 14:01:37 +0100

    nodejs-01

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,23 @@
-const generateContacts = async (number) => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+const generateContacts = async (number) => {
+    try {
+
+        let data = await readContacts();
+        if (!Array.isArray(data)) {
+          data = [];
+        };
+
+        for (let i = 0; i < number; i++){
+            data.push(createFakeContact());
+        }
+
+        await writeContacts(data);
+    } catch (err) {
+        console.error(err);
+    }
+};
 
 generateContacts(5);
--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,18 @@
-export const removeLastContact = async () => {};
+import { writeContacts } from "../utils/writeContacts.js";
+import { readContacts } from "../utils/readContacts.js";
+
+export const removeLastContact = async () => {
+    try {
+        const contacts = await readContacts();
+        if (contacts.length === 0) {
+            console.log('There are no entries in the contact list.');
+            return;
+        }
+
+        await writeContacts(contacts.slice(0, -1));
+    } catch(err) {
+        console.error(err);
+    }
+ };
 
 removeLastContact();
